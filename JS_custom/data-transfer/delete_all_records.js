86948659917f110e3b6829c1a9c8c5ad2f051fa7
naopen <naokizssm@gmail.com>
2023-06-22 17:21:01 +0900

    Add delete all records button to record index view

---
+++ JS_custom/data-transfer/delete_all_records.js
@@ -0,0 +1,62 @@
+(function () {
+	"use strict";
+	kintone.events.on('app.record.index.show', function (event) {
+
+		if (document.getElementById('my_index_button_01') !== null) {
+			return;
+		}
+
+		//レコード一覧画面にボタンを設置
+		var myIndexButton = document.createElement('button');
+		myIndexButton.id = 'delete_button';
+		myIndexButton.innerText = '⚠レコード一括削除⚠';
+		myIndexButton.className = 'kintoneplugin-button-normal';
+		myIndexButton.style.backgroundColor = "#ff4b00";
+		myIndexButton.style.color = "#ffffff";
+
+		// ボタンクリック時の処理
+		myIndexButton.addEventListener("click", async (event) => {
+			// 警告
+			if (!confirm('一覧のレコードを全件削除します。削除する件数は' + event.records.length + '件です。よろしいですか？'))
+				return;
+			// 再度警告
+			else if (!confirm('本当に' + event.records.length + '件のレコードを削除しますか？'))
+				return;
+			// 最終警告
+			else if (!confirm('本当に' + event.records.length + '件のレコードを削除しますか？\n\nこの操作は取り消せません。\n\nよろしいですか？'))
+				return;
+
+			//1.getAllRecordsをつかって対象のものを全件取得
+			const client = new KintoneRestAPIClient();
+			const appId = kintone.app.getId();
+			const res = await client.record.getAllRecords({ app: appId });
+			const obj = kintone.app.record.get();
+			const tblRecords = [];
+
+			//2.1で取得したレコードからレコード番号だけとりだし配列化する     
+			// 取得レコードのレコード番号
+			const targetRecords = res.map((r) => {  //  idだけ抽出(mapは、ある配列から新しい配列を作る機能です）
+				return {
+					id: r.$id.value,
+				};
+			});
+
+			console.log(targetRecords);
+
+			// //3.deleteAllRecordsを使って（2で抽出したIDをパラメータに渡し）、全件削除
+			// //レコード削除
+			// const res_dell = await client.record.deleteAllRecords({
+			// 	app: appId,
+			// 	records: targetRecords,
+			// });
+
+			//リロード
+			location.reload();
+			//アラート
+			alert("レコードを全件削除しました。");
+
+		});
+
+		kintone.app.getHeaderMenuSpaceElement().appendChild(myIndexButton);
+	});
+})();
