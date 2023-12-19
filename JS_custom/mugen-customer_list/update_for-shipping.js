da97f312fcee24d86167f0623b2fa2da85d3841b
naopen <naokizssm@gmail.com>
2023-12-19 18:11:29 +0900

    Update button text and functionality for changing customer status

--- JS_custom/mugen-customer_list/update_for-shipping.js
+++ JS_custom/mugen-customer_list/update_for-shipping.js
@@ -11,37 +11,88 @@
 
     const myIndexButton = document.createElement("button");
     myIndexButton.id = "my_index_button";
-    myIndexButton.innerText =
-      "【在庫端末】 ▶▶▶ 【顧客使用中端末】　　（このボタンを押すと今までマクロがやっていたことを瞬時に行えます）";
+    myIndexButton.innerText = "【顧客使用中端末】にする";
     myIndexButton.className = "kintoneplugin-button-normal";
+
+    function showInfo(title, message) {
+      return new Promise((resolve, reject) => {
+        Swal.fire({
+          title: title,
+          html: message,
+          confirmButtonText: 'OK',
+          confirmButtonColor: '#3498db',
+          width: 'auto',
+          heightAuto: true,
+          showCancelButton: true,
+          cancelButtonText: 'キャンセル',
+          cancelButtonColor: '#d33',
+          allowOutsideClick: false
+        }).then((result) => {
+          if (result.isConfirmed) {
+            resolve(true);
+          } else {
+            reject(new Error("キャンセルが選択されました"));
+          }
+        }
+        );
+      });
+    }
+
+    function showSuccess(title, message) {
+      return new Promise((resolve) => {
+        Swal.fire({
+          title: title,
+          html: message,
+          icon: 'success',
+          confirmButtonText: 'OK',
+          confirmButtonColor: '#3498db',
+          width: 'auto',
+          heightAuto: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            resolve(true);
+          }
+        });
+      }
+      );
+    }
+
+    function showAlert(title, message) {
+      Swal.fire({
+        title: title,
+        html: message,
+        icon: 'error',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#3498db',
+        width: 'auto',
+        heightAuto: true
+      });
+    }
 
     // ボタンクリック時の処理
     myIndexButton.onclick = () => {
       javascript: (function () {
-        const fromStatus = "在庫端末",
-          doAction = "宅急便に荷物を渡した",
-          condition = { fromStatus: fromStatus, doAction: doAction },
+        // 更新前ステータスは指定しない
+        const doAction = "顧客使用中端末にする",
+          // fromStatus = "在庫端末",
+          // condition = { fromStatus: fromStatus, doAction: doAction },
+          condition = { doAction: doAction },
           getUrl = kintone.api.url("/k/v1/records", !0),
           putUrl = kintone.api.url("/k/v1/records/status", !0);
         let currentQuery = kintone.app.getQueryCondition();
-        currentQuery != "" && (currentQuery += " and ");
+        // currentQuery != "" && (currentQuery += " and ");
         const getBody = {
           app: kintone.app.getId(),
           fields: ["$id"],
           query:
             currentQuery +
-            'ステータス = "' +
-            condition.fromStatus +
-            '" limit 100',
+            'limit 100',
         };
-
-        if (!event.size) {
-          alert('レコードがありません');
-          return;
-        }
-
+        console.log("クエリ：" + getBody.query);
         kintone
+          // レコード一覧取得
           .api(getUrl, "GET", getBody)
+          // レコード一覧取得成功時の処理 
           .then(function (resp) {
             const ids = resp.records.map(function (record) {
               return record.$id.value;
@@ -51,26 +102,56 @@
               putBody.records.push({ id: id, action: condition.doAction });
             }),
               console.log(putBody);
-            let message =
-              "　　　　＊ " +
+            // もし件数が0件ならエラーを新しく投げる
+            if (ids.length === 0) {
+              throw new Error("ステータスを更新する端末がありません");
+            }
+            let title =
               ids.length +
-              "台＊\n\nの端末のステータス更新をします。よろしいですか？\n\n⚠注意！　同時実行最大数は100台です。⚠\n⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤\n";
-            (message +=
-              "【在庫端末】 ▶▶▶ 【顧客使用中端末】\n　現在のステータス：" +
-              condition.fromStatus),
-              (message += "\n　実行アクション：" + condition.doAction);
-            const result = window.confirm(message);
-            return result ? kintone.api(putUrl, "PUT", putBody) : reject();
+              "台の端末の<br>ステータス更新をします。<br>よろしいですか？",
+              message = "⚠注意！　同時実行最大数は100台です。⚠<hr><br>実行アクション：" + condition.doAction;
+            // ステータス更新の確認処理
+            showInfo(title, message).then(() => {
+              return kintone.api(putUrl, "PUT", putBody);
+            })
+              // OKボタン押下でステータス更新を実行
+              // ステータス更新成功時 (その1) 一括更新が成功したら、チェックボックスを外す
+              .then(function (resp) {
+                const putBody = {
+                  app: kintone.app.getId(),
+                  records: [],
+                };
+                resp.records.forEach(function (record) {
+                  putBody.records.push({
+                    id: record.id,
+                    record: {
+                      "to_AIR_U解約予定": {
+                        value: [],
+                      },
+                    },
+                  });
+                });
+                console.log(putBody);
+                const putUrl = kintone.api.url("/k/v1/records", !0);
+                return kintone.api(putUrl, "PUT", putBody);
+              })
+              // ステータス更新成功時 (その2) 成功メッセージを表示
+              .then(function (resp) {
+                console.log(resp);
+                showSuccess("ステータスの一括更新が完了しました", "⚠注意！⚠　101台以上の端末がある場合は再度同じ操作を実行してください").then(() => {
+                  window.location.reload();
+                });
+              })
+              // ステータス更新失敗時の処理 一括更新が失敗したら、エラーメッセージを表示
+              .catch(function (error) {
+                console.log("ステータス更新失敗：" + error.message);
+                showAlert(error.message, "処理を中断しました。");
+              });
           })
-          .then(function (resp) {
-            console.log(resp),
-              alert(
-                "ステータスの一括更新が完了しました\n\n⚠注意！⚠　101台以上の端末がある場合は再度同じ操作を実行してください"
-              ),
-              window.location.reload();
-          })
+          // レコード一覧取得失敗時の処理
           .catch(function (error) {
-            console.log(error), alert("処理を中断しました。");
+            console.log("レコード一覧取得失敗：" + error.message);
+            showAlert(error.message, "処理を中断しました。");
           });
       })();
     };
